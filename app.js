dc34eeae9f03b1e9cf33b6d022c92226f7167a67
Kevin Parent <kevin.pparent@gmail.com>
2025-05-19 21:22:07 -0400

    M

--- app.js
+++ app.js
@@ -407,7 +407,7 @@ document.getElementById("photoInput").addEventListener("change", function(event)
 
     document.getElementById("resultatsPhoto").innerHTML = `
       <div class="alert alert-info">
-        <button type="button" class="btn-close float-end" onClick="closeThis()" aria-label="Close"></button>
+        <button type="button" class="btn-close float-end" aria-label="Close"></button>
         <h3><small>Les informations ici sont à titre indicatives et doivent servir UNIQUEMENT à vous guider dans votre recherche. 
             Vous devez toujours valider votre identification par vous même</small></h3>
         <strong>Suggestions de mots-clés :</strong><br>
