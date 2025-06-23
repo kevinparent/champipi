d144fd508460b47dc8b6ff77fec8776f9b3caacc
Kevin Parent <kevin.pparent@gmail.com>
2025-06-23 10:44:52 -0400

    m

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'champipi-offline-cache-v1-2025-06-23-953';
+const CACHE_NAME = 'champipi-offline-cache-v1-2025-06-23-1044';
 const FILES_TO_CACHE = [
   '/champipi/',
   '/champipi/index.html',
